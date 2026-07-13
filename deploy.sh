2dd5b75021328995dff3dde196221f4166aa40f3
ajospino <ajospino21@gmail.com>
2026-07-13 02:25:13 -0500

    Updated deploy script and created destroy script, as to have ease of use of the whole code

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ cd terraform/
 tofu init 
 tofu plan -out cp2.tfplan
 tofu apply --auto-approve cp2.tfplan
+az aks get-credentials --resource-group $(tofu output -raw resource_group_name) --name $(tofu output -raw kubernetes_cluster_name)
 
 cd ../ansible
 ansible-playbook playbook-create-image.yml
-ansible-playbook -i inventory -u azureuser --private-key ../.ssh/az-cp2-private-key.pem playbook-app.yml
+ansible-playbook playbook-app.yml
+ansible-playbook playbook-aks.yml
---
+++ destroy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd terraform/
+tofu destroy --auto-approve
